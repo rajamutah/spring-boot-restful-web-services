82d2c0d92d640cd14aefef1a3bbd67ab6308f8cb
Meenatchi Vijayakumar <meenatchivijayakumar123@gmail.com>
2025-09-27 23:34:47 +0530

    creating the spring-boot-app

---
+++ src/main/java/com/springboot_learning/spring/boot/restful/web/services/SpringBootRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_learning.spring.boot.restful.web.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_learning/spring/boot/restful/web/services/SpringBootRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_learning.spring.boot.restful.web.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
